ffd3f6bdfe4ddb8cf56bf71e0769e7b4f43fc083
Morris Viitanen <morris.viitanen@helsinki.fi>
2022-11-11 14:15:56 +0200

    Kassapäätteen tetsejä edistetty

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,11 +1,54 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
-    def test_alkutilanne_oikein(self):
-        self.assertEqual(self.kassassa_rahaa, 100000)
-        self.assertEqual(self.edulliset, 0)
-        self.assertEqual(self.maukkaat, 0)
+    def setUp(self) -> None:
+        self.kassapaate = Kassapaate()
+        self.maksukortti = Maksukortti(1000)
 
+    def test_alkurahat_oikein(self):
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
 
-        
+    def test_edullisia_nolla_alussa(self):
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_maukkaita_nolla_alussa(self):
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_edullinen_kateisosto_kasvattaa_kassaa(self):
+        self.kassapaate.syo_edullisesti_kateisella(240)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_edullinen_kateisosto_vaihtorahat_oikein(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kateisella(250), 10)
+
+    def test_edullinen_kateisosto_rahat_ei_riita(self):
+        self.kassapaate.syo_edullisesti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_maukas_kateisosto_kasvattaa_kassaa(self):
+        self.kassapaate.syo_maukkaasti_kateisella(400)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_maukas_kateisosto_vaihtorahat_oikein(self):
+        self.assertEqual(self.kassapaate.syo_maukkaasti_kateisella(450), 50)
+
+    def test_maukas_kateisosto_rahat_ei_riita(self):
+        self.kassapaate.syo_maukkaasti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_edullinen_kortilla_tarpeeksi_rahaa(self):
+        self.assertEqual(self.kassapaate.syo_edullisesti_kortilla(self.maksukortti), True)
+
+    def test_edullinen_korttiosto_toimii(self):
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 7.60 euroa")
+
+    def test_maukas_korttiosto_toimii(self):
+        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 6.00 euroa")
