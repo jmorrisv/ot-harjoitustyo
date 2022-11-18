418c2086cbd57d830ff1c06e5b1f7b6a5d384352
Morris Viitanen <morris.viitanen@helsinki.fi>
2022-11-18 12:41:58 +0200

    luotu config.py

--- src/entities/task.py
+++ src/entities/task.py
@@ -1,10 +1,11 @@
 import uuid
+from datetime import datetime
 
 class Task:
     """Siivottavaa kohdetta kuvaava luokka
     
     Attributes:
-        subject: Merkkijono, joka kuvaa siivottavaa kohdetta
+        name: Merkkijono, joka kuvaa siivottavaa kohdetta
         frequency_d: Kokonaisluku, joka kuvaa siivousvälin päiviä
         frequency_h: Kokonaisluku, joka kuvaa siivousvälin tunteja
         frequency_m: Kokonaisluku, joka kuvaa siivousvälin minuutteja
@@ -12,11 +13,11 @@ class Task:
         task_id: Merkkijono, joka kuvaa kohteen id:tä
     """
 
-    def __init__(self, subject, frequency_d=0, frequency_h=0, frequency_m=0, dirty=False, task_id=None):
+    def __init__(self, name, frequency_d=0, frequency_h=0, frequency_m=0, dirty=False, task_id=None):
         """Luokan konstruktori, joka luo uuden kohteen.
 
         Args:
-            subject: Merkkijono, joka kuvaa siivottavaa kohdetta
+            name: Merkkijono, joka kuvaa siivottavaa kohdetta
             frequency_d:
                 Vapaaehtoinen, oletusarvoltaan 0.
                 Kokonaisluku, joka kuvaa siivousvälin päiviä
@@ -34,9 +35,10 @@ class Task:
                 Merkkijono, joka kuvaa kohteen id:tä.
         """
 
-        self.subject = subject
+        self.name = name
         self.frequency_d = frequency_d
         self.frequency_h = frequency_h
         self.frequency_m = frequency_m
         self.dirty = dirty
         self.task_id = task_id or str(uuid.uuid4())
+        self.frequency = datetime
--- src/repositories/task_repository.py
+++ src/repositories/task_repository.py
@@ -1,2 +1,42 @@
-from pathlib import Path
-from entities.task import Task
+class TaskRepository:
+
+    """Luokka joka vastaa tiedon tallentamisesta tietokantaan ja sen hakemisesta."""
+
+    def __init__(self, file_path):
+
+        """Luokan konstruktori.
+        
+        Args:
+            file_path: Polku tiedostoon, johon luodut Taskit tallennetaan.
+        """
+
+        self._file_path = file_path
+
+    def find_tasks(self):
+        
+        """Palauttaa kaikki tallennetut Taskit
+        
+        Returns:
+            Palauttaa Task-olioita sisältävän listan.
+        """
+
+        return self._read
+
+    def _read(self):
+        tasks = []
+        self._ensure_file_exists()
+
+        with open(self._file_path, encoding="utf-8") as file:
+            for row in file:
+                row = row.replace("\n", "")
+                parts = row.split(";")
+
+                task_id = parts[0]
+                name = parts[1]
+                frequency = parts[2]
+                
+
+
+
+
+        
